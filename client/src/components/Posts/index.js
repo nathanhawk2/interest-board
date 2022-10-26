91caaab0a0277961a289fc0842752983b26da2cf
Sreggie <darren.rogers06@gmail.com>
2022-10-26 13:35:36 -0400

    added post folder and file

---
+++ client/src/components/Posts/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PostList = ({ post, theme }) => {
+  if (!post.length) {
+    return <h3>No Posts Yet</h3>;
+  }
+
+  return (
+    
+  );
+};
+
+export default PostList;
