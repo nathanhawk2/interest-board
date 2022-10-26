d0bdf1216ddcc6dad4864cefdeed54c1b0194fe3
Nesha Whit <neshawhitmire@gmail.com>
2022-10-26 12:02:00 -0400

    fixed login layout

---
+++ client/src/pages/NewPost.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import React from 'react';
+import { useQuery } from '@apollo/client';
+// Utilities
+import Auth from '../utils/auth';
+import { QUERY_USERS } from '../utils/queries';
+// Components
+import UserList from '../components/UserList';
+
+
+const NewPost = () => {
+
+
+
+
+    return (
+        <main>
+            <div>
+                <form>
+                    <h1>Create a Post</h1>
+                    <input type="search">Pick a Theme</input>
+                </form>
+
+            </div>
+            <div >
+                {renderUserList()}
+            </div>
+        </main>
+    );
+};
+
+export default NewPost;
