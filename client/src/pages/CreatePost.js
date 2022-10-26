3cb4480c04a39e2e69cb4e6212a124068dc8a35c
Will D <will.dean0103@gmail.com>
2022-10-26 13:22:09 -0400

    .

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,19 +1,20 @@
 import React from "react";
 
 export default function createPost() {
-  return (   
-<div style={{display:'flex', justifyContent:'center', margin:'5px'}} className='createPost' >
-  <form>
-    <h2 style={{margin:'5px'}} className="header">Create a Post</h2>
-    <div style={{margin:'3px'}}>
-      <input type="text" id="theme" placeholder="Theme"></input>
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', margin: '5px' }} className='createPost' >
+      <form id="form" >
+        <h2 style={{ margin: '5px' }} className="header">Create a Post</h2>
+        <div style={{ margin: '3px' }}>
+          <input type="text" id="theme" placeholder="Theme"></input>
+        </div>
+        <div style={{ margin: '3px', textAlign: 'center' }}>
+          <input type="text" placeholder="Compose your post here" id="post"></input>
+        </div>
+        <button id="box" className="postBtn" style={{ justifyContent: 'center' }}>Submit Post</button>
+      </form>
+      <div id="outputtext"></div>
     </div>
-    <div style={{margin:'3px', textAlign:'center'}}>
-      <input type="text" placeholder="Compose your post here" id="post"></input>
-    </div>
-    <button className="postBtn" style={{justifyContent:'center'}}>Submit Post</button>
-  </form>
-  
-</div>
   );
 }
+
